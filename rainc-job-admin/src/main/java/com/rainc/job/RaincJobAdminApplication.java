48033c2e6b0774377686406a38e9499eba7bb313
amerainc <1123015515@qq.com>
2021-07-03 09:52:46 +0800

    启动计时

--- rainc-job-admin/src/main/java/com/rainc/job/RaincJobAdminApplication.java
+++ rainc-job-admin/src/main/java/com/rainc/job/RaincJobAdminApplication.java
@@ -1,16 +1,23 @@
 package com.rainc.job;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.util.StopWatch;
 
 /**
  * @author rainc
  */
 @SpringBootApplication
+@Log4j2
 public class RaincJobAdminApplication {
 
 	public static void main(String[] args) {
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.start();
 		SpringApplication.run(RaincJobAdminApplication.class, args);
+		stopWatch.stop();
+		log.info(stopWatch.getLastTaskTimeMillis());
 	}
 
 }
