e16ba47c1f0a53a47d60ebc95d1d01f167a1e3b6
zhengyuchen.zyc <zhengyuchen.zyc@gongdao.com>
2023-03-01 15:39:27 +0800

    up

--- rainc-job-admin/src/main/java/com/rainc/job/config/CorsConfig.java
+++ rainc-job-admin/src/main/java/com/rainc/job/config/CorsConfig.java
@@ -4,7 +4,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
+/**
+ * 跨域配置
+ */
 @Configuration(proxyBeanMethods = false)
 public class CorsConfig implements WebMvcConfigurer {
     @Override
